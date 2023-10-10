0b48e3eb98ff463ae962e41505020c4c6750cd50
TheMLord <mikhail.berezin.03@mail.ru>
2023-10-10 14:12:07 +0500

    initial commit

---
+++ src/main/java/naumen/course/coffeeShop/WebApplicationForTheCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package naumen.course.coffeeShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationForTheCoffeeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationForTheCoffeeShopApplication.class, args);
+    }
+
+}
